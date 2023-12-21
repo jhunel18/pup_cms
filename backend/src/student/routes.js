4d2f1f4787dac4fd27c18ca3d0a18921c253f001
Jhunel Penaflorida <jhunelpenaflorida@gmail.com>
2023-12-21 10:15:16 +0800

    Initial commit for backend

---
+++ backend/src/student/routes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+const controller = require('./controller');
+
+const router = new Router();
+
+router.get("/", controller.getStudents);
+router.post("/", controller.addStudent);
+router.get("/:id", controller.getStudentById);
+router.delete("/:id", controller.removeStudent);
+router.put("/:id", controller.updateStudent);
+
+
+module.exports = router;
