7f6753166ff878c8f2aa7ff42d4bff9370807c42
Jhunel Penaflorida <jhunelpenaflorida@gmail.com>
2024-03-23 21:35:30 +0800

    Remove unnecessary files

--- backend/src/queries/authQuery.js
+++
@@ -1,8 +0,0 @@
-const emailExistsQuery = "SELECT * FROM users WHERE user_email = $1";
-const createUserQuery = "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *"
-// const addStudent = "INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)";
-
-module.exports = {
-   emailExistsQuery,
-   createUserQuery
-};
--- backend/src/queries/dashboardQuery.js
+++
@@ -1,6 +0,0 @@
-const getAllUsersQuery = "SELECT user_id, user_name FROM users";
-
-
-module.exports = {
-  getAllUsersQuery
-};
