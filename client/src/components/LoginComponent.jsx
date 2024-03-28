96f1b175ffd3babedf06be96a6c4c9e5ed286666
Jhunel Penaflorida <jhunelpenaflorida@gmail.com>
2024-03-28 22:32:04 +0800

    Update Login Page

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import { AuthenticationService } from '../services/AuthenticationService';
 import toast, { Toaster } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
-const LoginComponent = ({ onSubmit }) => {
-  const [email, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const LoginComponent = ({onSubmit}) => {
+  const [email, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleUsernameChange = (event) => {
@@ -16,14 +16,13 @@ const LoginComponent = ({ onSubmit }) => {
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
       const data = await AuthenticationService.login({ email, password });
       // Handle successful login based on your server's response
-      console.log('Login successful:', data);
+      console.log('Login successful');
       onSubmit(data); // Pass response data to App.js (optional)
       toast.success('Login successful'); // Display success toast
       navigate('/dashboard')
@@ -34,36 +33,53 @@ const LoginComponent = ({ onSubmit }) => {
   };
 
   return (
-    <div>
-      <Toaster position="top-center" /> {/* Add Toaster component */}
-      <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
-        <label htmlFor="username" className="text-sm font-medium">
-          Username:
-        </label>
-        <input
-          type="text"
-          id="email"
-          value={email}
-          onChange={handleUsernameChange}
-          className="rounded-md border border-gray-300 p-2 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-        />
-        <label htmlFor="password" className="text-sm font-medium">
-          Password:
-        </label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-          className="rounded-md border border-gray-300 p-2 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-        />
-        <button
-          type="submit"
-          className="bg-indigo-600 text-white rounded-md py-2 px-4 hover:bg-indigo-700"
-        >
-          Login
+    <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-200">
+    <Toaster position="top-center" /> {/* Add Toaster component */}
+      <h1 className="text-5xl font-semibold">Welcome Back</h1>
+      <p className="font-medium text-lg text-gray-500 mt-4">
+        Please enter your details
+      </p>
+      <div className="mt-8 ">
+        <div>
+          <label className="text-lg font-medium">Email</label>
+          <input
+            id="email"
+            value={email}
+            onChange={handleUsernameChange}
+            className="w-full border-2 border-gray-100 rounded-xl p-3 mt-1 bg-transparent"
+            placeholder="Enter your Email"
+          />
+        </div>
+        <div>
+          <label className="text-lg font-medium">Password</label>
+          <input
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            className="w-full border-2 border-gray-100 rounded-xl p-3 mt-1 bg-transparent"
+            placeholder="Enter your Password"
+            type="password"
+          />
+        </div>
+        <div className="mt-8 flex justify-between items-center">
+          <div>
+            <input type="checkbox" id="remember" />
+            <label htmlFor="remember" className="ml-2 font-medium text-base">
+              Remember Me
+            </label>
+          </div>
+          <button className="font-medium text-base text-blue-500">
+            Forgot Password
+          </button>
+        </div>
+      </div>
+      <div className="mt-8">
+        <button 
+        className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out w-full rounded-md py-3 bg-violet-500 text-white text-lg font-bold"
+        onClick={handleSubmit}>
+          Sign In
         </button>
-      </form>
+      </div>
     </div>
   );
 };
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import LoginLayout from '../shared/components/Layouts/LoginLayout'
-import LoginForm from '../components/LoginForm';
+import LoginComponent from '../components/LoginComponent';
 
 
 const LoginPage = () => {
@@ -9,19 +9,10 @@ const LoginPage = () => {
 };
   return (
     <LoginLayout>
-        {/* <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <div className="max-w-md w-full space-y-8">
-                <h1>Login</h1>
-                <LoginComponent onSubmit={handleLogin} />
-            </div>  
-        </div> */}
         <div className='flex w-full h-screen'>
           <div className='w-full flex items-center justify-center'>
-            <LoginForm onSubmit={handleLogin}/>
+            <LoginComponent onSubmit={handleLogin}/>
           </div>
-          {/* <div className='hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200'>
-            <div className='w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full' />
-          </div> */}
         </div>
     </LoginLayout>
   )
